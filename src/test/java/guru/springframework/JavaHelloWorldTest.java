d96b7e76a2612b4f658704518bc6bb70a9cdfdb1
puneet mishra <puneetmishra545@gmail.com>
2022-06-28 18:40:10 -0400

    Added TestNG test

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void testGetHello() {
+    }
+}
