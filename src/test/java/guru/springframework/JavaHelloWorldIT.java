c9097a3ddc152f041929b361cc230606ae988f0b
puneet mishra <puneetmishra545@gmail.com>
2022-06-28 19:18:32 -0400

    Added failsafe

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaHelloWorldIT {
+
+    @Test
+    void getHello() {
+        System.out.println("IT Ran...");
+    }
+}
