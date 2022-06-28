c91ccda280d22fbae5ac40df7a3af609e98bee29
puneet mishra <puneetmishra545@gmail.com>
2022-06-28 13:49:07 -0400

    Added junit4 and 5 test

---
+++ src/test/java/guru/springframework/Junit5FauxTest.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5FauxTest {
+
+    @Test
+    void someTestForJunit5() {
+        System.out.println("I Ran....");
+    }
+}
